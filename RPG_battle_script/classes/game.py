79f09b814664857d532b1bd8b4c79bfbe7681c13
souvikb07 <saouvik01@gmail.com>
2019-05-19 23:05:27 +0530

    damage functions done

--- RPG_battle_script/classes/game.py
+++ RPG_battle_script/classes/game.py
@@ -13,41 +13,41 @@ class bcolors():
 # create class for player
 class Person():
     def __init__(self, hp, mp, atk, df, magic):
-            """
-            Parameters
-            ----------
-            hp : num
-                Health Point
-            mp : num
-                Magic Points
-            atk : num
-                Attack Points
-            df : num
-                Defence Points
-            magic : str
-                Magic Type
-            
-            Returns
-            -------
-            maxhp : num
-                Max Health Point of Player
-            hp : num
-                Health Point of Player
-            maxmp : num
-                Max Magic Points of Payer
-            mp : num
-                Magic Points of Payer
-            atkl : num
-                Low Attack Points of Player
-            atkh : num
-                High Attack Points of Player
-            df : num
-                Defence Points of Player
-            magic : str
-                Magic Type of Player
-            action : str
-                Action taken by Player
-            """
+        """
+        Parameters
+        ----------
+        hp : num
+            Health Point
+        mp : num
+            Magic Points
+        atk : num
+            Attack Points
+        df : num
+            Defence Points
+        magic : str
+            Magic Type
+        
+        Returns
+        -------
+        maxhp : num
+            Max Health Point of Player
+        hp : num
+            Health Point of Player
+        maxmp : num
+            Max Magic Points of Payer
+        mp : num
+            Magic Points of Payer
+        atkl : num
+            Low Attack Points of Player
+        atkh : num
+            High Attack Points of Player
+        df : num
+            Defence Points of Player
+        magic : str
+            Magic Type of Player
+        action : str
+            Action taken by Player
+        """
         self.maxhp = hp
         self.hp = hp
         self.maxmp = mp
@@ -56,7 +56,7 @@ class Person():
         self.atkh = atk + 10
         self.magic = magic
         self.action = ["Attack", "Magic"]
-    
+    # Function to generate damage points
     def generate_damage(self):
         """
         Parameters
@@ -64,6 +64,37 @@ class Person():
 
         Returns
         ----------
-         Range of attack : num
+         Range of attack damage : num
         """
         return random.randrange(self.atkl, self.atkh)
+
+    # Function to generate spell damage points
+    def generate_spell_damage(self, i):
+        """
+        Parameters
+        ----------
+        i : num
+            Index of the magic list
+        Returns
+        ----------
+         Range of magic damage : num
+        """
+        mgl = self.magic[i]["dmg"] - 5
+        mgh = self.magic[i]["dmg"] + 5
+        return random.randrange(mgl, mgh)
+    
+    # Function to generate Players take damage points
+    def take_damage(self, dmg):
+        """
+        Parameters
+        ----------
+        dmg : num
+            Damage Points
+        Returns
+        ----------
+         Health Point after damage : num
+        """
+        self.hp - = dmg
+        if self.hp < 0:
+            self.hp = 0
+        return self.hp
---
+++ RPG_battle_script/main.py
@@ -0,0 +1,7 @@
+from classes.game import Person, bcolors
+
+# Name of Magics Spells
+# Magic
+fire = Spell("Fire", 10, 600, "black")
+thunder = Spell("Thunder", 10, 600, "black")
+blizzard = Spell("Blizzard", 10, 600, "black")
